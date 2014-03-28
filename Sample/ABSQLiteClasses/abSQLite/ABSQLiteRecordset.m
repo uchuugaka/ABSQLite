3c2b73499ff6d07f8999b74a332a6a728a6df5f7
Aaron L. Bratcher <aaronbratcher1@gmail.com>
2014-03-28 14:17:46 -0400

    removed warnings

--- Sample/ABSQLiteClasses/abSQLite/ABSQLiteRecordset.m
+++ Sample/ABSQLiteClasses/abSQLite/ABSQLiteRecordset.m
@@ -16,21 +16,20 @@
 	BOOL eof;
 	BOOL closed;
 	sqlite3_stmt *dbps;
-	sqlite3* db;
+	sqlite3 *db;
 	dispatch_queue_t dbAccessQueue;
-	
+    
 	BOOL columnNamesSetup;
 	NSMutableDictionary *columnNameToIndexMap;
 }
 
 
-- (id) init {
+- (id)init {
 	self = [super init];
 	return self;
 }
 
-
-- (id) initWithSQLitePreparedStatement: (sqlite3_stmt *) initial_dbps db: (sqlite3*) initial_db  dispatchQueue:(dispatch_queue_t) dispatchQueue isForwardOnly:(BOOL) isForwardOnly {
+- (id)initWithSQLitePreparedStatement:(sqlite3_stmt *)initial_dbps db:(sqlite3 *)initial_db dispatchQueue:(dispatch_queue_t)dispatchQueue isForwardOnly:(BOOL)isForwardOnly {
 	self = [super init];
 	db = initial_db;
 	dbps = initial_dbps;
@@ -40,131 +39,125 @@
 	recordNumber = 0;
 	columnNamesSetup = NO;
 	[self setupColumnNames];
-	
+    
 	return self;
 }
 
-- (void) dealloc {
+- (void)dealloc {
 	[self close];
 }
 
 - (void)setupColumnNames {
 	if (!columnNameToIndexMap) {
-		columnNameToIndexMap =[NSMutableDictionary dictionary];
+		columnNameToIndexMap = [NSMutableDictionary dictionary];
 	}
-	
+    
 	_fieldCount = sqlite3_column_count(dbps);
-	
+    
 	int columnIdx = 0;
 	for (columnIdx = 0; columnIdx < _fieldCount; columnIdx++) {
-		columnNameToIndexMap[[@(sqlite3_column_name(dbps, columnIdx)) lowercaseString]] = @(columnIdx);
+		columnNameToIndexMap[[@(sqlite3_column_name(dbps, columnIdx))lowercaseString]] = @(columnIdx);
 	}
-	
+    
 	columnNamesSetup = YES;
 }
 
-- (int)columnIndexForName:(NSString*)columnName {
+- (int)columnIndexForName:(NSString *)columnName {
 	columnName = [columnName lowercaseString];
 	NSNumber *n = columnNameToIndexMap[columnName];
-	
+    
 	if (n) {
 		return [n intValue];
 	}
-	
-//	NSLog(@"Warning: I could not find the column named '%@'.", columnName);
-	
+    
+	//	NSLog(@"Warning: I could not find the column named '%@'.", columnName);
+    
 	return -1;
 }
 
-
-- (BOOL) bof {
+- (BOOL)bof {
 	return (recordNumber == 0);
 }
 
-- (void) close {
+- (void)close {
 	dispatch_sync(dbAccessQueue, ^{
-		if (!closed) {
-			closed = YES;
-			sqlite3_finalize (dbps);
+	    if (!closed) {
+	        closed = YES;
+	        sqlite3_finalize(dbps);
 		}
 	});
 }
 
-
-- (BOOL) eof {
+- (BOOL)eof {
 	return eof;
 }
 
-- (int) fieldCount {
+- (int)fieldCount {
 	return _fieldCount;
 }
 
-
-- (id<ABField>) fieldWithName:(NSString*)fieldName {
+- (id <ABField> )fieldWithName:(NSString *)fieldName {
 	int i = [self columnIndexForName:fieldName];
-	if(i == -1)
+	if (i == -1)
 		return nil;
-	
-	return [self fieldAtIndex: i];
+    
+	return [self fieldAtIndex:i];
 }
 
-- (id<ABField>) fieldAtIndex: (int) index {
-	__block ABSQLiteField* field;
-	
+- (id <ABField> )fieldAtIndex:(int)index {
+	__block ABSQLiteField *field;
+    
 	dispatch_sync(dbAccessQueue, ^{
-		NSString* fieldName = @(sqlite3_column_name(dbps, index));
-		field = [[ABSQLiteField alloc] initWithSQLitePreparedStatement:dbps withName:fieldName dispatchQueue:dbAccessQueue withfieldIndex:index];
+	    NSString *fieldName = @(sqlite3_column_name(dbps, index));
+	    field = [[ABSQLiteField alloc] initWithSQLitePreparedStatement:dbps withName:fieldName dispatchQueue:dbAccessQueue withfieldIndex:index];
 	});
-
+    
 	return field;
 }
 
-- (void) moveFirst {
+- (void)moveFirst {
 	dispatch_sync(dbAccessQueue, ^{
-		sqlite3_reset(dbps);
-		eof = NO;
-		recordNumber = 0;
+	    sqlite3_reset(dbps);
+	    eof = NO;
+	    recordNumber = 0;
 	});
 	[self moveNext];
 }
 
-- (void) moveLast {
-	
+- (void)moveLast {
 }
 
-- (void) moveNext {
+- (void)moveNext {
 	dispatch_sync(dbAccessQueue, ^{
-		if (!eof) {
-			int dbrc = sqlite3_step(dbps);
-			
-			if (dbrc == SQLITE_ROW)
+	    if (!eof) {
+	        int dbrc = sqlite3_step(dbps);
+            
+	        if (dbrc == SQLITE_ROW)
 				recordNumber++;
-			else {
-				eof = YES;
-				
-				if (dbrc < 100) {
-					NSString* errorMessage = [[NSString alloc] initWithUTF8String:sqlite3_errmsg(db)];
-					NSLog([NSString stringWithFormat:@"!!!! SQLite Error (%@) !!!! ",errorMessage]);
+	        else {
+	            eof = YES;
+                
+	            if (dbrc < 100) {
+	                // TODO: throw an exception
 				}
 			}
 		}
 	});
 }
 
-- (void) movePrevious {
-	
+- (void)movePrevious {
 }
 
-- (int) recordCount {
+- (int)recordCount {
 	if (_recordCount > 0) {
 		return _recordCount;
 	}
-	
-	if([self eof]) {
+    
+	if ([self eof]) {
 		_recordCount = recordNumber;
 		return _recordCount;
 	}
-	
+    
 	int currentRecordNumber = recordNumber;
 	int maxRecordNumber = 0;
 	[self moveFirst];
@@ -176,10 +169,9 @@
 	while (recordNumber < currentRecordNumber) {
 		[self moveNext];
 	}
-	
+    
 	_recordCount = maxRecordNumber;
 	return maxRecordNumber;
 }
-
 
 @end
